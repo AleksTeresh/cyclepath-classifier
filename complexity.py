bf5160d9f3c80f4a260419d1da24b343ef96239f
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2020-09-14 16:54:05 +0300

    Add complexity dict for all 11 problem types for (un)directed paths & cycles

---
+++ complexity.py
@@ -0,0 +1,63 @@
+CONST = "O(1)"
+GLOBAL = "Θ(n)"
+ITERATED_LOG = "Θ(log* n)"
+UNSOLVABLE = " - "
+
+complexities = {
+  "cycles": {
+    "directed": {
+      "A": CONST,
+      "B": CONST,
+      "C": CONST,
+      "D": CONST,
+      "E": ITERATED_LOG,
+      "F": ITERATED_LOG,
+      "G": ITERATED_LOG,
+      "H": GLOBAL,
+      "I": GLOBAL,
+      "J": UNSOLVABLE,
+      "K": UNSOLVABLE,
+    },
+    "undirected": {
+      "A": CONST,
+      "B": ITERATED_LOG,
+      "C": GLOBAL,
+      "D": GLOBAL,
+      "E": ITERATED_LOG,
+      "F": GLOBAL,
+      "G": GLOBAL,
+      "H": GLOBAL,
+      "I": GLOBAL,
+      "J": UNSOLVABLE,
+      "K": UNSOLVABLE,
+    }
+  },
+  "paths": {
+    "directed": {
+      "A": CONST,
+      "B": CONST,
+      "C": CONST,
+      "D": CONST,
+      "E": ITERATED_LOG,
+      "F": ITERATED_LOG,
+      "G": ITERATED_LOG,
+      "H": GLOBAL,
+      "I": GLOBAL,
+      "J": UNSOLVABLE,
+      "K": CONST,
+    },
+    "undirected": {
+      "A": CONST,
+      "B": ITERATED_LOG,
+      "C": GLOBAL,
+      "D": GLOBAL,
+      "E": ITERATED_LOG,
+      "F": GLOBAL,
+      "G": GLOBAL,
+      "H": GLOBAL,
+      "I": GLOBAL,
+      "J": UNSOLVABLE,
+      "K": CONST,
+    }
+  }
+}
