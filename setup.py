b91451fe3de6b94fe57cec0d67eb544b4b648875
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2021-01-19 17:50:53 +0200

    Change setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,25 @@
-#!/usr/bin/env python
-import os
+import pathlib
 from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
 
 setup(
-    name='cyclepath-classifier',
-    version='0.1.0',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/AleksTeresh/cyclepath-classifier',
-    author='Aleksandr Tereshchenko',
-    author_email='aleksandr.tereshch@gmail.com',
-    license='MIT',
-    packages=['cyclepath_classifier'],
-    install_requires=[],
+    name="cyclepath-classifier",
+    version="0.1.0",
+    description="A command-line tool for automatically calculating round complexity of LCL problems in cycles and paths based on their description in the node-edge-checkable formalism",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AleksTeresh/cyclepath-classifier",
+    author="Aleksandr Tereshchenko",
+    author_email="aleksandr.tereshch@gmail.com",
+    license="MIT",
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
-
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
-
-        'Programming Language :: Python :: 3.8',
-    ]
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=["cyclepath_classifier"],
+    include_package_data=True,
 )
