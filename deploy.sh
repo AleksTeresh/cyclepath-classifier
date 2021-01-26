d26f82f184ea9fe72fd1f913f06a5d5af2ae3529
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2021-01-26 11:28:56 +0200

    Add deployment bash script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# build
+python3 setup.py sdist bdist_wheel
+
+# check
+twine check dist/*
+
+# publish
+twine upload dist/*
+
+# cleanup
+rm -rf ./build
+rm -rf ./dist
+rm -rf ./cyclepath_classifier.egg-info
+
